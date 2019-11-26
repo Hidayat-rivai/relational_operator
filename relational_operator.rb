017860397450f1e26e7f1cda93668b7786de157e
Hidayat Riva'i <stu.dent.palawija@gmail.com>
2019-11-26 21:46:08 +0700

    relational operator

---
+++ relational_operator.rb
@@ -0,0 +1,14 @@
+A = 10
+B = 9
+
+puts "A = #{A}"
+puts "B = #{B}"
+puts "A == B -> #{A == B}"
+puts "A != B -> #{A != B}"
+puts "A > B -> #{A > B}"
+puts "A >= B -> #{A >= B}"
+puts "A < B -> #{A < B}"
+puts "A <= B -> #{A <= B}"
+puts "A <=> B -> #{A <=> B}"
+puts "B <=> A -> #{B <=> A}"
+puts "A <=> 10 -> #{A <=> 10}"
